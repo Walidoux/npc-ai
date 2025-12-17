fc5ad1c457f44b2441c5f557eeed484b5dafcf0e
Walidoux <ma.walidkorchi@icloud.com>
2025-12-18 00:40:08 +0100

    feat: new npc + improve dialogue box

---
+++ src/assets/npcs/npc_4/metadata.md
@@ -0,0 +1,7 @@
+---
+name: Walid
+personality: Web developer
+traits: ['smart', 'creative', 'passionate']
+---
+
+Very smart, always thinks out of the box.
--- src/components/dialogue-box.tsx
+++ src/components/dialogue-box.tsx
@@ -4,10 +4,12 @@ type DialogueBoxProps = {
 }
 
 export const DialogueBox = ({ displayedText, isTyping }: DialogueBoxProps) => (
-  <div className='arrow-down absolute bottom-46 left-8 w-70 rounded-xl border p-4'>
-    <p className='select-none text-xl leading-5 tracking-wide'>
-      {displayedText}
-      {Boolean(isTyping) && <span className='animate-pulse'>|</span>}
-    </p>
+  <div className='arrow-down absolute bottom-46 left-8 max-h-[200px] w-80 border'>
+    <div className='sidebar-scroll h-[calc(200px-24px)] overflow-y-auto px-4 py-2 pr-3 text-justify'>
+      <p className='select-none text-xl leading-5 tracking-wide'>
+        {displayedText}
+        {Boolean(isTyping) && <span className='animate-pulse'>|</span>}
+      </p>
+    </div>
   </div>
 )
