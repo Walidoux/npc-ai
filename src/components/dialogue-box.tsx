511291de275259d1a169fbae55ead7da9e9b68e3
Walidoux <ma.walidkorchi@icloud.com>
2025-12-18 00:43:46 +0100

    feat: add auto scroll down

--- src/components/dialogue-box.tsx
+++ src/components/dialogue-box.tsx
@@ -3,13 +3,25 @@ type DialogueBoxProps = {
   isTyping: boolean
 }
 
-export const DialogueBox = ({ displayedText, isTyping }: DialogueBoxProps) => (
-  <div className='arrow-down absolute bottom-46 left-8 max-h-[200px] w-80 border'>
-    <div className='sidebar-scroll h-[calc(200px-24px)] overflow-y-auto px-4 py-2 pr-3 text-justify'>
-      <p className='select-none text-xl leading-5 tracking-wide'>
-        {displayedText}
-        {Boolean(isTyping) && <span className='animate-pulse'>|</span>}
-      </p>
+export const DialogueBox = ({ displayedText, isTyping }: DialogueBoxProps) => {
+  const scrollRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight
+    }
+  }, [displayedText])
+
+  return (
+    <div className='arrow-down absolute bottom-46 left-8 max-h-[200px] w-80 border'>
+      <div
+        className='sidebar-scroll max-h-[calc(200px-24px)] overflow-y-auto px-4 py-2 pr-3'
+        ref={scrollRef}>
+        <p className='select-none text-xl leading-5 tracking-wide'>
+          {displayedText}
+          {Boolean(isTyping) && <span className='animate-pulse'>|</span>}
+        </p>
+      </div>
     </div>
-  </div>
-)
+  )
+}
